6ea81662a612209e785c0211b22b60d208a65976
Yousen Zhang <zysright@mail.ustc.edu.cn>
2019-06-26 22:52:10 +0200

    update config

--- FlowCorr/batch/submit_jobs_trg_ref.py
+++ FlowCorr/batch/submit_jobs_trg_ref.py
@@ -1,22 +1,29 @@
 import subprocess
-name = "submit_corr2D_trg_ref.jdl"
+
+tree = ["PromptD0", "NonPromptD0", "NonPromptD0"]
+treeNumber = 0
+#treeNumber = 2
+
+name = "submit_corr2D_trg_ref_%s.jdl" % tree[treeNumber]
 f = open(name, "w")
 
 command_lines = '''Universe   = vanilla
 GetEnv     = True
 Executable = submit_corr2D_trg_ref.sh
-Arguments  = small.list.00
-Log        = log/submit_corr2D_trg_ref.$(Process).log
-Output     = out/submit_corr2D_trg_ref.$(Process).out
-Error      = err/submit_corr2D_trg_ref.$(Process).err
+Arguments  = 00 %d
+Log        = log/submit_corr2D_trg_ref_%s.$(Process).log
+Output     = out/submit_corr2D_trg_ref_%s.$(Process).out
+Error      = err/submit_corr2D_trg_ref_%s.$(Process).err
+requirements = (OpSysAndVer =?= "CentOS7")
++JobFlavour           = "workday"
 Queue
-'''
+''' % (treeNumber, tree[treeNumber],tree[treeNumber],tree[treeNumber])
 
-for i in range(1, 80):
+for i in range(1, 90):
    temp = '''
-Arguments  = small.list.%02d
+Arguments  = %02d %d
 Queue
-   ''' % i
+   ''' % (i, treeNumber)
    command_lines += temp
 
 f.write(command_lines)
