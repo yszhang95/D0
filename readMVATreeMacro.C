6e5f0164ea42892e3e8ed742f407f309e1efdb54
Yousen Zhang <zysright@mail.ustc.edu.cn>
2019-07-08 20:25:16 +0200

    clean

--- readMVATreeMacro.C
+++
@@ -1,9 +0,0 @@
-#include "TROOT.h"
-//#include "readMVAtree.C"
-
-void readMVATreeMacro()
-{
-   gROOT->ProcessLine(".L readMVAtree.C++");
-   gROOT->ProcessLine("readMVAtree(1)");
-   gROOT->ProcessLine("readMVAtree(2)");
-}
