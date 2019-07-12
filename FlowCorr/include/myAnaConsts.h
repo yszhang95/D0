440bd284689f313b0941f9c29ff3644bd3a4f143
Yousen Zhang <zysright@mail.ustc.edu.cn>
2019-07-12 06:07:54 +0200

    add corr2D_trg_ref_mult

--- FlowCorr/include/myAnaConsts.h
+++ FlowCorr/include/myAnaConsts.h
@@ -46,11 +46,11 @@ namespace ana{
       1.87, 1.88, 1.9, 1.92, 1.96, 2.0};
    
    const int nPt_NPD0_pPb = 3;
-   const double ptbin_NPD0_pPb[nPt+1] = {2., 4., 6., 8.};
+   const double ptbin_NPD0_pPb[nPt_NPD0_pPb+1] = {2., 4., 6., 8.};
    const int nPt_PD0_pPb = 8;
-   const double ptbin_PD0_pPb[nPt+1] = {1.5, 2.4, 3.0, 3.5, 4.0, 5.0, 6.0, 7.0, 8.0};
+   const double ptbin_PD0_pPb[nPt_PD0_pPb+1] = {1.5, 2.4, 3.0, 3.5, 4.0, 5.0, 6.0, 7.0, 8.0};
    const int nPt_PD0_pp = 3;
-   const double ptbin_PD0_pp[nPt+1] = {2., 4., 6., 8.};
+   const double ptbin_PD0_pp[nPt_PD0_pp+1] = {2., 4., 6., 8.};
    
    const int nY = 1;
    const double ybin[nY+1] = {0., 2.0};
