e5b39c854501a988deffff81f43f6aa69afbf3d7
Yousen Zhang <zysright@mail.ustc.edu.cn>
2019-07-14 09:21:33 +0200

    update REF

--- FlowCorr/macros/proj1D_HM_PD0_Process.C
+++ FlowCorr/macros/proj1D_HM_PD0_Process.C
@@ -1,7 +1,8 @@
 void proj1D_HM_PD0_Process(const char* input_d0= "../data/corr2D_PAHM185-250_d0ana_HM_2.0.root",
-      const char* input_ref = "data/corr2D_ref_d0ana.root", 
+      const char* input_ref = "../data/corr2D_PAHM185-250_REF_HM_2.0.root", 
 const float y=2.0, const string dataset="PAHM1-6")
 {
+   const double deltaEtaBound = 1;
 
    gErrorIgnoreLevel = kWarning;
    gStyle->SetOptStat(0);
@@ -28,7 +29,7 @@ const float y=2.0, const string dataset="PAHM1-6")
    double v2_DCA_err[ana::nMass][nPt];
 
    TFile* f1 = new TFile(input_d0);
-//   TFile* f2 = new TFile(input_ref);
+   TFile* f2 = new TFile(input_ref);
 
    TH3D* hDcaVsMassAndMva[nPt]; 
    
@@ -62,8 +63,8 @@ const float y=2.0, const string dataset="PAHM1-6")
    TH1D* hDeltaPhi[ana::nMass][nPt];
 
    int negBinMin = 0;
-   int negBinMax = h2DSignal_D0[0][0]->GetXaxis()->FindBin(-1.)-1 ;
-   int posBinMin = h2DSignal_D0[0][0]->GetXaxis()->FindBin(1.)+1 ;
+   int negBinMax = h2DSignal_D0[0][0]->GetXaxis()->FindBin(-1.* deltaEtaBound)-1 ;
+   int posBinMin = h2DSignal_D0[0][0]->GetXaxis()->FindBin(1.* deltaEtaBound)+1 ;
    int posBinMax = h2DSignal_D0[0][0]->GetXaxis()->GetNbins()+1;
 
    for(int imass=0; imass<ana::nMass; imass++){
@@ -96,15 +97,14 @@ const float y=2.0, const string dataset="PAHM1-6")
       }
    }
 
-   /*
    TH2D* h2DSignal_Ref = (TH2D*) f2->Get("hSignal_Ref");
    TH2D* h2DBackground_Ref = (TH2D*) f2->Get("hBackground_Ref");
    TH1D* hMult = (TH1D*) f2->Get("hMult");
 
 
    int negBinMin_Ref = 0;
-   int negBinMax_Ref = h2DSignal_Ref->GetXaxis()->FindBin(-1.)-1 ;
-   int posBinMin_Ref = h2DSignal_Ref->GetXaxis()->FindBin(1.)+1 ;
+   int negBinMax_Ref = h2DSignal_Ref->GetXaxis()->FindBin(-1.*deltaEtaBound)-1 ;
+   int posBinMin_Ref = h2DSignal_Ref->GetXaxis()->FindBin(1.*deltaEtaBound)+1 ;
    int posBinMax_Ref = h2DSignal_Ref->GetXaxis()->GetNbins()+1;
 
    TH1D* hNeg_Ref_Signal;
@@ -125,16 +125,15 @@ const float y=2.0, const string dataset="PAHM1-6")
    hDeltaPhi_Ref_bkg->SetName("deltaPhi_Ref_bkg");
    hDeltaPhi_Ref_bkg->Add(hPos_Ref_Background);
 
-   int center = hDeltaPhi_Ref_bkg->FindBin(0.);
+   int center = h2DBackground_Ref->FindBin(0., 0.);
    hDeltaPhi_Ref->Divide(hDeltaPhi_Ref_bkg);
-   hDeltaPhi_Ref->Scale(hDeltaPhi_Ref_bkg->GetBinContent(center)/hDeltaPhi_Ref_bkg->GetBinWidth(1));
+   hDeltaPhi_Ref->Scale(h2DBackground_Ref->GetBinContent(center)/hDeltaPhi_Ref_bkg->GetBinWidth(1));
 
    delete hNeg_Ref_Signal;
    delete hPos_Ref_Signal;
    delete hNeg_Ref_Background;
    delete hPos_Ref_Background;
    delete hDeltaPhi_Ref_bkg;
-   */
    
    std::string function = "[0]/(TMath::Pi()*2)*(1+2*([1]*TMath::Cos(x)+[2]*TMath::Cos(2*x)+[3]*TMath::Cos(3*x)))";
 
@@ -147,6 +146,8 @@ const float y=2.0, const string dataset="PAHM1-6")
          func.SetParameter(1, 0.1);
          func.SetParameter(2, 0.1);
          func.SetParameter(3, 0.1);
+
+         hDeltaPhi[imass][ipt]->SetMarkerStyle(20);
 
          hDeltaPhi[imass][ipt]->Fit(&func, "q");
          hDeltaPhi[imass][ipt]->Fit(&func, "q");
@@ -177,34 +178,46 @@ const float y=2.0, const string dataset="PAHM1-6")
             str.erase(0, 1);
          }
          c_deltaPhi[imass][ipt]->Print(Form("../plots/d0ana/y%.1f/%s_deltaPhi_mass%d_pt%d.png", 
-                     y, str.c_str(), imass, ipt));
+                  y, str.c_str(), imass, ipt));
 
          delete c_deltaPhi[imass][ipt];
       }
    }
 
-   /*
    TCanvas* cRef = new TCanvas("cRef", "", 550, 450);
    TF1 func_ref("deltaPhi_Ref", function.c_str(), -3.14159*0.5, 3.14159*1.5);
    func_ref.SetParameter(0, hDeltaPhi_Ref->GetMaximum());
    func_ref.SetParameter(1, 0.1);
    func_ref.SetParameter(2, 0.1);
    func_ref.SetParameter(3, 0.1);
+   hDeltaPhi_Ref->SetMarkerStyle(20);
    hDeltaPhi_Ref->Fit(&func_ref, "q");
    hDeltaPhi_Ref->Fit(&func_ref, "q");
    hDeltaPhi_Ref->Fit(&func_ref, "m q");
    auto fitResult = hDeltaPhi_Ref->Fit(&func_ref, "m S E");
-   cRef->Print(Form("plots/%s_ref_V2.png", input_ref));
+   std::string str = input_d0;
+   auto found = str.find("/");
+   while(found!=std::string::npos){
+      str.replace(found, 1, "_");
+      found = str.find("/");
+   }
+   while(str.at(0) == '.'){
+      str.erase(0, 1);
+   }
+   while(str.at(0) == '_'){
+      str.erase(0, 1);
+   }
+   cRef->Print(Form("../plots/%s_ref_V2.png", str.c_str()));
    delete cRef;
 
-   TGraphErrors* g_v2_DCA[nPt][nDca];
+   /*
+   TGraphErrors* g_v2_DCA[nPt];
    for(int ipt=0; ipt<nPt; ipt++){
-      for(int idca=0; idca<nDca; idca++){
-      g_v2_DCA[ipt][idca] = new TGraphErrors(ana::nMass);
-      g_v2_DCA[ipt][idca]->SetName(Form("g_v2_DCA_pt%d_dca%d", ipt, idca));
-         for(int imass=0; imass<ana::nMass; imass++){
-            double temp = v2_DCA[imass][ipt][idca]/ sqrt(func_ref.GetParameter(2));
-            double temp_err = temp* sqrt(pow(v2_DCA_err[imass][ipt][idca]/ v2_DCA[imass][ipt][idca], 2) 
+      g_v2_DCA[ipt] = new TGraphErrors(ana::nMass);
+      g_v2_DCA[ipt]->SetName(Form("g_v2_pt%d", ipt));
+      for(int imass=0; imass<ana::nMass; imass++){
+         double temp = v2_DCA[imass][ipt][idca]/ sqrt(func_ref.GetParameter(2));
+         double temp_err = temp* sqrt(pow(v2_DCA_err[imass][ipt][idca]/ v2_DCA[imass][ipt][idca], 2) 
                + pow(0.5*func_ref.GetParError(2)/ func_ref.GetParameter(2), 2));
 
             v2_DCA[imass][ipt][idca] = temp;
@@ -215,7 +228,9 @@ const float y=2.0, const string dataset="PAHM1-6")
          }
       }
    }
+   */
 
+   /*
    TH1D* hMass_DCA[nPt][nDca];
    int mvaBinMin, mvaBinMax, dcaBinMin, dcaBinMax;
    for(int ipt=0; ipt<nPt; ipt++){
