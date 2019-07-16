0668cc671c04ed57e2723273caa354c6d119619f
Yousen Zhang <zysright@mail.ustc.edu.cn>
2019-07-16 23:04:00 +0200

    update

---
+++ FlowCorr/batch/submit_corr2D_trg_pd0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "setup cmssw"
+cd /afs/cern.ch/user/y/yousen/public/pPb2016
+source setup_CMSSW_8_0_31
+cd CMSSW_8_0_31/src
+eval `scramv1 runtime -sh`
+
+# cd to work dir
+cd /afs/cern.ch/user/y/yousen/work/pPb2016/D0/FlowCorr/batch
+echo PWD: $PWD
+
+#OUTDIR=/afs/cern.ch/user/y/yousen/work/pPb2016/HM185-250-PD0-v2vspt
+#../bin/corr2D_trg_pd0 ../test/data_ref_HM.small.list PAHM1-6 ../eff/fEff.root $OUTDIR
+# run
+../bin/corr2D_trg_pd0 $1 $2 $3 $4 $5 $6 $7 $8
